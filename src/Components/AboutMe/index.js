9d343555b1fe32929d479947a046a66d770423a2
Ellis O'Dowd <riptideellis@gmail.com>
2023-06-30 20:39:12 -0700

    Added minterval to projects, also fixed project mobile

---
+++ src/Components/AboutMe/index.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+import "./styles.css";
+
+const AboutMe = () => {
+  return (
+    <div className="about-me-zone">
+      <h1>about me</h1>
+    </div>
+  );
+};
+
+export default AboutMe;
